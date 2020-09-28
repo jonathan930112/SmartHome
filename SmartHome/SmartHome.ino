d03c749c63e9f1a83d3653218174c1b1b467f776
jonathan930112 <mine19765craftj74561@gmail.com>
2020-09-28 15:19:04 +0800

    他會亂叫git status(蜂鳴器)

--- SmartHome/SmartHome.ino
+++ SmartHome/SmartHome.ino
@@ -1,14 +1,19 @@
 void setup() {
   // put your setup code here, to run once:
   pinMode(12,OUTPUT);
+  pinMode(11,OUTPUT);
+  pinMode(7,OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   digitalWrite(12,HIGH);
+  digitalWrite(11,LOW);
+  tone(7,1000,500);
   delay(500);
   digitalWrite(12,LOW);
   digitalWrite(11,HIGH);
+  tone(7,800,500);
   delay(500);
-  digitalWrite(11,LOW);
+  
 }
