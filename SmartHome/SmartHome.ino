090280a17c7cd6f341d308e21252cc1f7a05ae5c
jonathan930112 <mine19765craftj74561@gmail.com>
2020-09-28 15:49:22 +0800

    斷線後會叫

--- SmartHome/SmartHome.ino
+++ SmartHome/SmartHome.ino
@@ -10,18 +10,13 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   if(digitalRead(2)==HIGH){
-    Serial.println("HIGH");
+    digitalWrite(12,HIGH);
+    digitalWrite(11,LOW);
+    tone(7,1000,500);
+    delay(500);
+    digitalWrite(12,LOW);
+    digitalWrite(11,HIGH);
+    tone(7,800,500);
+    delay(500);
   }
-  else {
-    Serial.println("LOW");
-  }
-  digitalWrite(12,HIGH);
-  digitalWrite(11,LOW);
-  tone(7,1000,500);
-  delay(500);
-  digitalWrite(12,LOW);
-  digitalWrite(11,HIGH);
-  tone(7,800,500);
-  delay(500);
-  
 }
